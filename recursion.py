e91b37bd8928af8c2dfeba6ce449314c12fcace6
Nirali Rajpura <niralirajpura@Mac.lan>
2025-07-06 14:25:32 -0500

    done

--- recursion.py
+++ recursion.py
@@ -1,4 +1,7 @@
 # recursion
+
+# factorial 
+
 def fact(num, count=1, factorial=1):
     if count > num:
         return factorial #base case stops function.
@@ -6,3 +9,23 @@ def fact(num, count=1, factorial=1):
         return fact(num, count + 1, factorial * count) #fun calling it self (recursion)
 
 print(fact(5))  # Output: 120
+
+
+# fibonaci
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+       
+    return fibonacci(n - 1) + fibonacci(n - 2) # 
+    print(fibonacci)
+    # return fibonacci(n - 1) + fibonacci(n - 2) # 
+
+# print("10th Fibonacci number:", fibonacci(10))
+
+print(fibonacci(10))  
+
+# # Print the full series up to the 10th term (0 to 9)
+# print("Fibonacci series up to 10 terms:")
+# for i in range(10):
+#     print(fibonacci(i), end=" ") 
