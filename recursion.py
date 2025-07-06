b8b6d0e2364651c3a75df7e898b6aaf2544ba997
Nirali Rajpura <niralirajpura@Mac.lan>
2025-07-06 09:21:44 -0500

    recursion

--- recursion.py
+++ recursion.py
@@ -1,1 +1,8 @@
 # recursion
+def fact(num, count=1, factorial=1):
+    if count > num:
+        return factorial #base case stops function.
+    else:
+        return fact(num, count + 1, factorial * count) #fun calling it self (recursion)
+
+print(fact(5))  # Output: 120
