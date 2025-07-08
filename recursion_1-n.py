ad9ef88429e6808bdaa60c3b8b6e7ab692e557bc
Nirali Rajpura <niralirajpura@Mac.lan>
2025-07-08 17:17:25 -0500

    recursion example

--- recursion_1-n.py
+++ recursion_1-n.py
@@ -6,3 +6,15 @@ def number (n,i=1):
         return number(n,i+1)
 n = int(input("Please enter number till you want to print: "))
 number(n)
+
+"""
+output
+Please enter number till you want to print: 7
+1
+2
+3
+4
+5
+6
+7
+"""
