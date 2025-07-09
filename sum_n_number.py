b37c91a208d199d0e8824ed1a8cb464bfacd6eb9
Nirali Rajpura <niralirajpura@Mac.lan>
2025-07-09 00:10:07 -0500

    add

---
+++ sum_n_number.py
@@ -0,0 +1,21 @@
+def sum(n,i =0,add=0):
+    if(i>n):
+        return add
+    else:
+        add = add + i
+        print(add)
+        return sum(n,i+1,add)
+        
+
+print("sum of given numbers ",sum(3))
+
+"""
+Output: 
+
+0
+1
+3
+6
+sum of given numbers  6
+
+"""
